b26b8f3d8f2562e6dd33c955a96e65a3b4f45ea6
BadKernel <tonifenoy78@gmail.com>
2023-01-12 19:38:28 +0100

    Inicio

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+const Todo = require('./todo/routes/todo.routes');
+
+Todo.todoRoutes(app);
+
+app.listen(3000,'localhost',()=>{
+    console.log('server listening on port %s',3000);
+})
